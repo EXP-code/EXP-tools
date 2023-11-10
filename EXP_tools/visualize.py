78fed662efd9f8f4e431775f7332b00d68c03424
Arpit Arora <119627552+appy2806@users.noreply.github.com>
2023-11-10 14:46:19 -0500

    Update visualize.py
    Added a combined fx to generate field_grids. Older implementations of slice_fields will be removed soon.

--- EXP_tools/visualize.py
+++ EXP_tools/visualize.py
@@ -114,6 +114,72 @@ def spherical_avg_prop(basis, coefficients, time=0, radius=np.linspace(0.1, 600,
 
     return np.array(field), radius
 
+def return_fields_in_grid(basis, coefficients, times=[0], 
+                       projection='3D', proj_plane=0, 
+                       grid_lim=300, npoints=150,):
+
+    """
+    Returns the 2D or 3D field grids as a dict at each time step as a key.
+
+    Args:
+    basis (obj): object containing the basis functions for the simulation
+    coefficients (obj): object containing the coefficients for the simulation
+    times (list): list of the time to compute the field grid.
+    projection (str): the slice projection to plot. Can be '3D', XY', 'XZ', or 'YZ'.
+    proj_plane (float, optional): the value of the coordinate that is held constant in the 2D slice projection
+    npoints (int, optional): the number of grid points in each dimension
+    grid_limits (float, optional): the limits of the grid in the dimensions.
+
+    Returns:
+    heirarchical dict structure with for each time:
+        dict_keys(['d', 'd0', 'd1', 'dd', 'fp', 'fr', 'ft', 'p', 'p0', 'p1'])
+        where 
+        - 'd': Total density at each point.
+        - 'd0' : Density in the monopole term.
+        - 'd1' : Density in l>0 terms.
+        - 'dd': Density difference.
+        - 'fp': Force in the phi direction.
+        - 'fr': Force in the radial direction.
+        - 'ft': Force in the theta direction.
+        - 'p': Total potential at each point.
+        - 'p0': Potential in the monopole term.
+        - 'p1': Potential in l>0 terms.
+    """
+
+    assert projection in ['3D', 'XY', 'XZ', 'YZ'], "Invalid value for 'projection'. Must be one of '3D', 'XY', 'XZ', or 'YZ'."
+
+
+    
+    if projection == '3D':
+        pmin  = [-grid_lim, -grid_lim, -grid_lim]
+        pmax  = [grid_lim, grid_lim, grid_lim]
+        grid  = [npoints, npoints, npoints]
+        
+        ##create a projection grid along with it
+        x = np.linspace(-grid_lim, grid_lim, npoints)
+        xgrid = np.meshgrid(x, x, x)
+
+    else:
+        x = np.linspace(-grid_lim, grid_lim, npoints)
+        xgrid = np.meshgrid(x, x)
+        grid  = [npoints, npoints]
+        
+        if projection == 'XY':        
+            pmin  = [-grid_lim, -grid_lim, proj_plane]
+            pmax  = [grid_lim, grid_lim, proj_plane]
+            
+        elif projection == 'XZ':
+            pmin  = [-grid_lim,  proj_plane, -grid_lim]
+            pmax  = [grid_lim, proj_plane, grid_lim]
+
+        elif projection == 'YZ':
+            pmin  = [proj_plane,  -grid_lim, -grid_lim]
+            pmax  = [proj_plane, grid_lim, grid_lim]
+
+    
+    field_gen = pyEXP.field.FieldGenerator(times, pmin, pmax, grid)
+
+    return field_gen.volumes(basis, coefs), xgrid
 
 def slice_fields(basis, coefficients, time=0, 
                  projection='XY', proj_plane=0, npoints=300, 
