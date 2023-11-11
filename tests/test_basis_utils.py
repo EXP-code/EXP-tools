9abc3528259c3fa1ed5947cea551e24fb94bcd18
jngaravitoc <jngaravitoc@email.arizona.edu>
2023-11-11 15:56:54 -0500

    tests

---
+++ tests/test_basis_utils.py
@@ -0,0 +1,4 @@
+import numpy as np
+from EXPtools import basis_utils
+
+# test integration between functions! 
---
+++ tests/test_profiles.py
@@ -0,0 +1,6 @@
+import numpy as np
+from EXPtools import profiles
+
+# Test output of functions
+# Test document test functions 
+
