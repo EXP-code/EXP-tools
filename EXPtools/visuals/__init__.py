1dc320a95ac6fd0b4dfb900625e23140947983bc
jngaravitoc <jngaravitoc@email.arizona.edu>
2023-12-05 22:30:27 -0500

    adding visualization routines

---
+++ EXPtools/visuals/__init__.py
@@ -0,0 +1,8 @@
+from .projections import mollweide
+from .visualization import make_basis_plot
+from .visualization import find_field
+from .visualization import spherical_avg_prop
+from .visualization import return_fields_in_grid
+from .visualization import slice_fields
+from .visualization import slice_3d_fields
+
