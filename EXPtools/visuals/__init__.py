8c33c224eb7b7b82f7df706821d7ae9dcc2965cd
jngaravitoc <jngaravitoc@email.arizona.edu>
2023-12-10 22:27:23 -0500

    visualization -> visualize

--- EXPtools/visuals/__init__.py
+++ EXPtools/visuals/__init__.py
@@ -1,8 +1,8 @@
 from .projections import mollweide
-from .visualization import make_basis_plot
-from .visualization import find_field
-from .visualization import spherical_avg_prop
-from .visualization import return_fields_in_grid
-from .visualization import slice_fields
-from .visualization import slice_3d_fields
+from .visualize import make_basis_plot
+from .visualize import find_field
+from .visualize import spherical_avg_prop
+from .visualize import return_fields_in_grid
+from .visualize import slice_fields
+from .visualize import slice_3d_fields
 
