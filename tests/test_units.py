6c302b4fc04b10825a5964608b974e2162fa1dad
Hayden Foote <haydenfoote@email.arizona.edu>
2024-11-18 12:08:18 -0700

    fixed test for unit handling

--- tests/test_units.py
+++ tests/test_units.py
@@ -5,12 +5,12 @@ Tests for unit conversions
 import numpy as np
 import astropy.units as u
 import astropy.constants as const
-from EXPtools.utils.exp_units import define_exp_units
+from EXPtools.utils import units
 
 def test_define_exp_units():
 
 	tol = 1e-10
-	expL, expM, expV, expT = define_exp_units(M_phys=1.*u.Msun, R_phys=1.*u.kpc, G=const.G.to(u.kpc/(1e10*u.Msun)*u.km**2/u.s**2))
-	assert (np.abs(expT.value - (u.kpc/(u.km/u.s)).to(u.Gyr)) < tol), "Unit definitions in define_exp_units failed!"
+	expL, expM, expV, expT = units.define_exp_units(M_phys=1.*u.Msun, R_phys=1.*u.kpc, G=const.G.to(u.kpc/(1e10*u.Msun)*u.km**2/u.s**2))
+	assert (np.abs((1.*expV).to(u.km/u.s).value - 0.002073865296984421) < tol), "Unit definitions in define_exp_units failed!"
 
 	return None
