ebf0391bbd1e7a1f9c91d5511aa5097343e2aeb6
jngaravitoc <jngaravitoc@email.arizona.edu>
2026-01-09 14:42:19 -0500

    minor update in ios

--- EXPtools/ios/ios.py
+++ EXPtools/ios/ios.py
@@ -6,10 +6,8 @@ read gala coefficients into EXP
 read AGAMA coefficients into EXP
 """
 
-
-import numpy as np
 import pyEXP
-from EXPtools.basis.basis_utils import make_config
+from EXPtools.basis.basis_utils import write_config
 
 def exp_coefficients(coeffile, config, **kwargs):
     """
@@ -24,16 +22,6 @@ def exp_coefficients(coeffile, config, **kwargs):
     Have a cache reader
 
     """
-    if config is None:
-        config = make_config(kwargs['basis_id'],
-                             kwargs['numr'],
-                             kwargs['rmin'],
-                             kwargs['rmax'],
-                             kwargs['lmax'],
-                             kwargs['nmax'],
-                             kwargs['scale'],
-                             kwargs['modelname'],
-                             kwargs['cachename'])
     basis = pyEXP.basis.Basis.factory(config)
     coefs = pyEXP.coefs.Coefs.factory(coeffile)
     return basis, coefs
